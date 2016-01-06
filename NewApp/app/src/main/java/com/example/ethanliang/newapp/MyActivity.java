994604b1a452c7408c00676c8bbc51046a03ef17
Ethan Liang <ehliang@uwaterloo.ca>
2016-01-06 09:46:16 -0500

    Added radio button activity

--- NewApp/app/src/main/java/com/example/ethanliang/newapp/MyActivity.java
+++ NewApp/app/src/main/java/com/example/ethanliang/newapp/MyActivity.java
@@ -1,7 +1,10 @@
 package com.example.ethanliang.newapp;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.StringRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,7 +12,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
 
@@ -31,17 +36,36 @@ public class MyActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
-        final RadioGroup group1 = (RadioGroup) findViewById(R.id.orientation);
-        group1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        final Button button = (Button) findViewById(R.id.picturebutton);
+        final ImageView image = (ImageView) findViewById(R.id.prettypicture);
+        image.setImageResource(R.drawable.wdwd);
+        button.setText("Off");
+//
+//        final RadioGroup group1 = (RadioGroup) findViewById(R.id.orientation);
+//        group1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(RadioGroup group, int checkedId) {
+//                switch (checkedId) {
+//                    case R.id.horizontal:
+//                        group.setOrientation(LinearLayout.HORIZONTAL);
+//                        break;
+//                    case R.id.vertical:
+//                        group.setOrientation(LinearLayout.VERTICAL);
+//                        break;
+//                }
+//            }
+//        });
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                switch (checkedId) {
-                    case R.id.horizontal:
-                        group.setOrientation(LinearLayout.HORIZONTAL);
-                        break;
-                    case R.id.vertical:
-                        group.setOrientation(LinearLayout.VERTICAL);
-                        break;
+            public void onClick(View v) {
+                if (button.getText()=="Off") {
+                    image.setImageResource(R.drawable.wwww);
+                    button.setText("On");
+                }
+                 else{
+                        image.setImageResource(R.drawable.wdwd);
+                        button.setText("Off");
+
                 }
             }
         });
@@ -77,6 +101,8 @@ public class MyActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
+
+
 
 
 }
